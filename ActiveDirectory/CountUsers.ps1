045972b84cd7f3329ad33425dd1d50682310b053
battlechess1981 <52680784+battlechess1981@users.noreply.github.com>
2022-01-25 15:50:49 -0600

    Add files via upload

---
+++ ActiveDirectory/CountUsers.ps1
@@ -0,0 +1,7 @@
+﻿$ous = Get-ADOrganizationalUnit -Filter * -SearchBase "OU=People,OU=Student Computing Services,DC=UTSARR,DC=NET" | Select-Object -ExpandProperty DistinguishedName
+$ous | ForEach-Object{
+    [psobject][ordered]@{
+        OU = $_
+        Count = (Get-ADUser -Filter * -SearchBase "$_").count
+    }
+}
